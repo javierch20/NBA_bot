03a31dbf1c659f40be13389806f998d97bd17ea6
javierch20 <43756059+javierch20@users.noreply.github.com>
2026-03-07 18:29:34 -0500

    Update TELEGRAM_TOKEN assignment in bot.py
    Replaced TELEGRAM_TOKEN environment variable with a hardcoded token.

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 # ─── Configuración ────────────────────────────────────────────────
 
-TELEGRAM_TOKEN = os.environ[“TELEGRAM_TOKEN”]
+TELEGRAM_TOKEN = os.environ[8781787874:AAEnwgK03RWozmKbm0yHwKmjriZLrFRwMmk]
 CHAT_ID = os.environ[“CHAT_ID”]
 NOTIFY_HOUR = int(os.getenv(“NOTIFY_HOUR”, “19”)) # 19 = 7 pm
 NOTIFY_MINUTE = int(os.getenv(“NOTIFY_MINUTE”, “0”))
